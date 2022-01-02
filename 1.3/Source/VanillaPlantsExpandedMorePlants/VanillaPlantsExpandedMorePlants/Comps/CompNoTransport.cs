837231383e5d0581e06872f941440635f3e36ef0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-01-02 16:33:44 +0100

    v0.1

---
+++ 1.3/Source/VanillaPlantsExpandedMorePlants/VanillaPlantsExpandedMorePlants/Comps/CompNoTransport.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace VanillaPlantsExpandedMorePlants
+{
+    public class CompNoTransport : ThingComp
+    {
+
+
+
+        public CompProperties_NoTransport Props
+        {
+            get
+            {
+                return (CompProperties_NoTransport)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            if (this.parent.IsHashIntervalTick(Props.tickInterval))
+            {
+               if(this.parent.Map == null)
+                {
+                   
+                        this.parent.Destroy();
+                   
+                }
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
+
---
+++ 1.3/Source/VanillaPlantsExpandedMorePlants/VanillaPlantsExpandedMorePlants/Comps/CompProperties/CompProperties_NoTransport.cs
@@ -0,0 +1,17 @@
+﻿
+using Verse;
+
+namespace VanillaPlantsExpandedMorePlants
+{
+    public class CompProperties_NoTransport : CompProperties
+    {
+        public int tickInterval = 2000;
+
+        public CompProperties_NoTransport()
+        {
+            this.compClass = typeof(CompNoTransport);
+        }
+
+
+    }
+}
