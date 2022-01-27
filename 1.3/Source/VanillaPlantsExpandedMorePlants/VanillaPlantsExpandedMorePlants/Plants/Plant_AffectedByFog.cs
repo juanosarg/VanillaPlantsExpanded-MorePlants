2a2899355a5c2e784486054cc1d8639d2494c494
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-01-27 08:41:07 +0100

    v0.1

---
+++ 1.3/Source/VanillaPlantsExpandedMorePlants/VanillaPlantsExpandedMorePlants/Plants/Plant_AffectedByFog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+
+using Verse;
+
+namespace VanillaPlantsExpandedMorePlants
+{
+    public class Plant_AffectedByFog : Plant
+    {
+
+        public override float GrowthRate
+        {
+            get
+            {
+                if (this.Blighted)
+                {
+                    return 0f;
+                }
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_Temperature * this.GrowthRateFactor_Light * this.GrowthRateFactor_Fog;
+            }
+        }
+
+        public float GrowthRateFactor_Fog
+        {
+            get
+            {
+                if (this.Map.weatherManager.curWeather.defName.Contains("fog")|| this.Map.weatherManager.curWeather.defName.Contains("Fog"))
+                {
+                    return 1.5f;
+                }
+                else return 1f;
+            }
+        }
+    }
+}
---
+++ 1.3/Source/VanillaPlantsExpandedMorePlants/VanillaPlantsExpandedMorePlants/Plants/Plant_AffectedBySnow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+
+using Verse;
+
+namespace VanillaPlantsExpandedMorePlants
+{
+    public class Plant_AffectedBySnow : Plant
+    {
+
+        public override float GrowthRate
+        {
+            get
+            {
+                if (this.Blighted)
+                {
+                    return 0f;
+                }
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_Temperature * this.GrowthRateFactor_Light * this.GrowthRateFactor_Snow;
+            }
+        }
+
+        public float GrowthRateFactor_Snow
+        {
+            get
+            {
+                if (this.Map.weatherManager.SnowRate > 0)
+                {
+                    return 1.5f;
+                }
+                else return 1f;
+            }
+        }
+    }
+}
