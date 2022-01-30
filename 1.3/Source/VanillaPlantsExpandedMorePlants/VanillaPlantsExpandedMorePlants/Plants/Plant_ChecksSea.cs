ae58b4199ce60a06783b87e521823991b25e33f0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-01-30 15:11:58 +0100

    v0.1

---
+++ 1.3/Source/VanillaPlantsExpandedMorePlants/VanillaPlantsExpandedMorePlants/Plants/Plant_ChecksSea.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+
+using Verse;
+
+namespace VanillaPlantsExpandedMorePlants
+{
+    public class Plant_ChecksSea : Plant
+    {
+
+        const int radius = 6;
+        int numberOfSea = 0;
+
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            int num = GenRadial.NumCellsInRadius(radius);
+            for (int i = 0; i < num; i++)
+            {
+                IntVec3 c = this.Position + GenRadial.RadialPattern[i];
+                if (c.InBounds(map))
+                {
+                    TerrainDef terrain = c.GetTerrain(map);
+
+                    if (terrain != null && terrain.IsWater && !terrain.IsRiver)
+                    {
+                        numberOfSea++;
+                    }
+
+                }
+
+
+
+
+            }
+
+        }
+
+        public override float GrowthRate
+        {
+            get
+            {
+                if (this.Blighted)
+                {
+                    return 0f;
+                }
+                if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map, false))
+                {
+                    return 0f;
+                }
+                return this.GrowthRateFactor_Fertility * this.GrowthRateFactor_Temperature * this.GrowthRateFactor_Light * this.GrowthRateFactor_Sea;
+            }
+        }
+
+        public float GrowthRateFactor_Sea
+        {
+            get
+            {
+
+                return 1f + (0.01f * numberOfSea);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look<int>(ref this.numberOfSea, "numberOfSea", 0, false);
+
+        }
+
+        public override string GetInspectString()
+        {
+            if (GrowthRateFactor_Sea == 1f)
+            {
+                return base.GetInspectString() + "\n" + "VCE_NoSeaNearby".Translate();
+
+            }
+            else
+            {
+                return base.GetInspectString() + "\n" + "VCE_SeaTilesNearby".Translate(numberOfSea);
+            }
+
+
+
+        }
+
+
+    }
+}
